5d6e5d05b7a4f2ea75153f34bbb5809675a412ce
vlzh <vlzh@pm.me>
2020-01-31 11:38:51 +0600

    Rename python file

--- echo_bot.py
+++ echo_bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from dotenv import load_dotenv
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("echo-bot")
@@ -11,17 +11,26 @@ def echo(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=update.message.text,
-        reply_to_message_id=update.message.message_id
+        reply_to_message_id=update.message.message_id,
     )
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello!")
 
 
 def main():
     load_dotenv()
     token = os.environ.get("ACCESS_TOKEN")
+    # Создаем экземпляр класса Updater
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
+    # Создаем 2 хендлера
     echo_handler = MessageHandler(Filters.text, echo)
     dispatcher.add_handler(echo_handler)
+    start_handler = CommandHandler("start", start)
+    dispatcher.add_handler(start_handler)
+    # Начинаем запрашивать обновления от Telegram Bot API
     updater.start_polling()
 
 
