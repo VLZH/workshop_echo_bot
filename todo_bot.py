b2232c08ed50c38859e32d4589e5632f896abc4b
vlzh <vlzh@pm.me>
2020-01-31 09:58:00 +0600

    v2

--- todo_bot.py
+++ todo_bot.py
@@ -1,45 +1,28 @@
 import os
 import logging
 from dotenv import load_dotenv
-from telegram import Bot
+from telegram.ext import Updater, MessageHandler, Filters
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("echo-bot")
 
-max_id = 0
+
+def echo(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text,
+        reply_to_message_id=update.message.message_id
+    )
 
 
 def main():
-    global max_id
     load_dotenv()
     token = os.environ.get("ACCESS_TOKEN")
-    bot = Bot(token)
-    while True:
-        logger.debug("start request")
-        # Мы хотим получать только новые апдейты, поэтому мы завели переменную max_id
-        # и вызываем метод getUpdates с параметром offset + timeout
-        updates = bot.getUpdates(offset=max_id + 1, timeout=60)
-        # В этой переменной мы сохраним максимальный id из полученных сейчас updates
-        max_id_in_updates = 0
-        for upd in updates:
-            logger.debug(f"new update with id: {upd.update_id}")
-            # Может быть так, что запустив бота мы получим апдейты
-            # которые произошли во время его "спячки", мы их просто отфильтруем следующим условием
-            # потому как max_id будет изменен только после обработки первых апдейтов
-            if max_id != 0:
-                bot.sendMessage(
-                    upd.message.chat_id,
-                    upd.message.text,
-                    reply_to_message_id=upd.message.message_id,
-                )
-            max_id_in_updates = (
-                upd.update_id
-                if upd.update_id > max_id_in_updates
-                else max_id_in_updates
-            )
-        if max_id_in_updates and max_id != max_id_in_updates:
-            max_id = max_id_in_updates
-            logger.debug(f"max id is changed to {max_id_in_updates}")
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
+    echo_handler = MessageHandler(Filters.text, echo)
+    dispatcher.add_handler(echo_handler)
+    updater.start_polling()
 
 
 if __name__ == "__main__":
